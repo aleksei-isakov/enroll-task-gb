e7f19313988bab2ee00c3db33b84ef189cd4e9fa
Alex Isakov <a.isakov@runetsoft.ru>
2024-04-15 14:30:58 +0300

    подключение зависимойстей и создание базы формы

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,50 @@
 <template>
-  <div class="hello">
-    Форма
+  <div>
+    <dx-select-box v-model="selectedTeam" :items="teamOptions" placeholder="Выберите команду"></dx-select-box>
+    <dx-select-box v-model="selectedFrequency" :items="frequencyOptions" placeholder="Выберите регулярность"></dx-select-box>
+    <div>
+      <dx-date-box v-model="selectedDate" placeholder="Выберите дату" :isValid="isValidDate"></dx-date-box>
+      <dx-select-box v-model="selectedDayOfWeek" :items="dayOfWeekOptions" placeholder="Выберите день недели" :showClearButton="true" :acceptCustomValue="true" :valueExpr="'value'" :displayExpr="'name'"></dx-select-box>
+    </div>
   </div>
 </template>
 
 <script>
+import 'devextreme/dist/css/dx.light.css';
+import DxSelectBox from 'devextreme-vue/select-box';
+import DxDateBox from 'devextreme-vue/date-box';
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    DxSelectBox,
+    DxDateBox,
+  },
+  data() {
+    return {
+      selectedTeam: null,
+      selectedFrequency: null,
+      selectedDate: null,
+      selectedDayOfWeek: [],
+      teamOptions: ['Analize', 'Vacuum'],
+      frequencyOptions: ['Регулярно', 'Однократно'],
+      dayOfWeekOptions: [
+        { name: 'Понедельник', value: 1 },
+        { name: 'Вторник', value: 2 },
+        { name: 'Среда', value: 3 },
+        { name: 'Четверг', value: 4 },
+        { name: 'Пятница', value: 5 },
+        { name: 'Суббота', value: 6 },
+        { name: 'Воскресенье', value: 7 }
+      ]
+    };
+  },
+  computed: {
+    isValidDate() {
+
+      return true;
+    }
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
