2bf15a9cf3320f45d4681a030ef22e89fde64094
Alex Isakov <a.isakov@runetsoft.ru>
2024-04-15 17:26:17 +0300

    добавлены базовые стили, переработан лейаут, добавлены лейблы, отключение произвольных значений в инпутах

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,69 @@
 <template>
-  <div>
-    <dx-select-box v-model="selectedTeam" :items="teamOptions" placeholder="Выберите команду"></dx-select-box>
-    <dx-select-box v-model="selectedFrequency" :items="frequencyOptions" placeholder="Выберите регулярность"></dx-select-box>
+  <div class="main-wrapper">
+    <div class="input-container">
+      <label class="label" for="team-select">Выберите команду</label>
+      <dx-select-box v-model="selectedTeam"
+                     :items="teamOptions"
+                     placeholder=""
+                     id="team-select"
+      />
+    </div>
+
+    <div class="input-container">
+      <label class="label" for="regularity-select">Выберите регулярность</label>
+      <dx-select-box v-model="selectedFrequency"
+                   :items="frequencyOptions"
+                   placeholder=""
+                   id="regularity-select"
+      >
+    </dx-select-box>
+    </div>
+
+    <div class="date-input-block">
+
+      <div class="input-container">
+      <label class="label" for="date-select">Дата</label>
+      <dx-date-box v-model="selectedDate"
+                   type="date"
+                   id="date-select"
+                   :min="currentDate"
+                   :acceptCustomValue="false"
+                   :onValueChanged="onDateChange"
+      >
+      </dx-date-box>
+      </div>
+
+      <div v-if="selectedFrequency === 'Регулярно'"
+           class="input-container"
+      >
+        <label class="label" for="day-select">День недели</label>
+        <dx-select-box v-model="selectedDayOfWeek"
+                     :items="dayOfWeekOptions"
+                     id="day-select"
+                     placeholder=""
+                     :showClearButton="true"
+                     :acceptCustomValue="false"
+                     :valueExpr="'value'"
+                     :displayExpr="'name'"
+                     :onValueChanged="onDayOfWeekChange"
+        >
+        </dx-select-box>
+      </div>
+
+      <div class="input-container">
+        <label class="label" for="time-select">Время</label>
+        <input type="time"
+             v-model="selectedTime"
+             id="time-select"
+             placeholder="Выберите время"
+             @onValueChanged="onTimeChange"
+        >
+      </div>
+    </div>
+
     <div>
-      <dx-date-box v-model="selectedDate" placeholder="Выберите дату" :isValid="isValidDate"></dx-date-box>
-      <dx-select-box v-model="selectedDayOfWeek" :items="dayOfWeekOptions" placeholder="Выберите день недели" :showClearButton="true" :acceptCustomValue="true" :valueExpr="'value'" :displayExpr="'name'"></dx-select-box>
+      <dx-button text="Сохранить" @click="validateAndSave"></dx-button>
+      <dx-button text="Отмена" @click="clearForm"></dx-button>
     </div>
   </div>
 </template>
@@ -13,18 +72,26 @@
 import 'devextreme/dist/css/dx.light.css';
 import DxSelectBox from 'devextreme-vue/select-box';
 import DxDateBox from 'devextreme-vue/date-box';
+import DxButton from 'devextreme-vue/button';
+// import DxValidator, {
+//   DxRequiredRule,
+//   DxPatternRule
+// } from 'devextreme-vue/validator';
 
 export default {
   components: {
     DxSelectBox,
     DxDateBox,
+    DxButton,
   },
   data() {
     return {
       selectedTeam: null,
       selectedFrequency: null,
       selectedDate: null,
-      selectedDayOfWeek: [],
+      selectedDayOfWeek: null,
+      selectedTime: null,
+      currentDate: new Date(),
       teamOptions: ['Analize', 'Vacuum'],
       frequencyOptions: ['Регулярно', 'Однократно'],
       dayOfWeekOptions: [
@@ -34,32 +101,61 @@ export default {
         { name: 'Четверг', value: 4 },
         { name: 'Пятница', value: 5 },
         { name: 'Суббота', value: 6 },
-        { name: 'Воскресенье', value: 7 }
+        { name: 'Воскресенье', value: 0 }
       ]
     };
   },
-  computed: {
-    isValidDate() {
+  methods: {
+    onDateChange(value) {
+      if (value && this.selectedDayOfWeek) {
+        this.selectedDayOfWeek = null;
+      }
+    },
+    onDayOfWeekChange(value) {
+      if (value && this.selectedDate) {
+        this.selectedDate = null;
+      }
+    },
+    onTimeChange() {
 
-      return true;
+    },
+    validateAndSave() {
+      const isValid = this.selectedFrequency === 'Однократно' ? this.selectedDate && this.selectedTime : this.selectedDate && this.selectedDayOfWeek && this.selectedTime;
+      isValid ? alert('Данные успешно сохранены') : alert('Неверно заполненные поля')
+    },
+    clearForm() {
+      this.selectedFrequency = null;
+      this.selectedDate = null;
+      this.selectedDayOfWeek = null;
+      this.selectedTime = null;
     }
+  },
+  computed: {
+      // тут будут валидаторы //
   }
 };
 </script>
 
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.main-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.date-input-block {
+  display: flex;
+  justify-content: space-between;
+  width: 70%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.input-container {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  text-align: left;
 }
-a {
-  color: #42b983;
+.label {
+  font-size: 13px;
+  opacity: 0.7;
 }
 </style>
